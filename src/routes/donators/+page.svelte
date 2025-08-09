654f31d43436361813c3b9dce1e236e19adec455
raynecloudy <rayne@raynec.dev>
2025-08-08 22:55:56 -0700

    donator list

--- src/routes/donators/+page.svelte
+++ src/routes/donators/+page.svelte
@@ -10,7 +10,9 @@
 <p>this page contains a list of people who have donated money to me via patreon. i'm so thankful for everyone who has given me real monies!! you have idea how much it means to me &lt;33</p>
 <p>if you want to donate to me, check out my patreon <a href="https://patreon.com/raynecloudy/shop">products</a> and/or <a href="https://patreon.com/raynecloudy/membership">memberships</a>!</p>
 <ul>
+  <li class="rainbow_donator"><a href="https://playfulmathematician.com">bringupyourpost</a></li>
   <li class="rainbow_donator"><a href="https://dumorando.com">dumorando</a></li>
+  <li><a href="https://milosantos.com">Milo</a></li>
   <li><a href="https://samv.me">shock59</a></li>
 </ul>
 <p>thank you all!!!!!</p>
