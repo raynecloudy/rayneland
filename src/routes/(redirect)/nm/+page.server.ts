33cef0a0a58159e0f1651312af10719563567cc6
raynecloudy <contact@raynec.dev>
2025-11-13 21:37:26 -0800

    fix nekomide redirects

--- src/routes/(redirect)/nm/+page.server.ts
+++ src/routes/(redirect)/nm/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from "@sveltejs/kit";
 
 export const load = async () => {
-  redirect(307, "https://github.com/raynecloudy/nekomide");
+  redirect(307, "https://github.com/nekomide");
 };
