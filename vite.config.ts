818ef9deaaa7893dc864f9f51012d76e7b5a948a
raynecloudy <contact@raynec.dev>
2025-11-17 15:44:45 -0800

    add new domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: ["raynec.dev"],
+		allowedHosts: ["raynec.dev", "rayne.page"],
 		port: 1234
 	}
 });
