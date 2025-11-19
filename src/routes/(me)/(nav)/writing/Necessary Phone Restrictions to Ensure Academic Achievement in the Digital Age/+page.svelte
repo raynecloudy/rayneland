92a65b249f0afb008e806ec3dff90394853944f6
raynecloudy <contact@raynec.dev>
2025-11-19 15:52:13 -0800

    fix pdf path

--- src/routes/(me)/(nav)/writing/Necessary Phone Restrictions to Ensure Academic Achievement in the Digital Age/+page.svelte
+++ src/routes/(me)/(nav)/writing/Necessary Phone Restrictions to Ensure Academic Achievement in the Digital Age/+page.svelte
@@ -3,10 +3,9 @@
   <meta name="og:description" content="phone use in the classroom is a very personal issue for me, so writing this essay wasn't that much of a struggle.">
   <meta name="og:title" content="Necessary Phone Restrictions to Ensure Academic Achievement in the Digital Age">
   <meta name="og:type" content="website">
-  <meta name="og:url" content="https:///writing/Necessary Phone Restrictions to Ensure Academic Achievement in the Digital Age">
+  <meta name="og:url" content="/writing/Necessary Phone Restrictions to Ensure Academic Achievement in the Digital Age">
 </svelte:head>
 
 <h1>Necessary Phone Restrictions to Ensure Academic Achievement in the Digital Age</h1>
 <p>i easily consider this the best essay i have ever written. phone use in the classroom is a very personal issue for me, so writing this essay wasn't that much of a struggle. i was able to find very reliable sources and make choices based off of my own observations.</p>
-<p>you can read the essay <a href="/Necessary Phone Restrictions to Ensure Academic Achievement in the Digital Age.pdf">here</a>.</p>
-<sub>my teacher said it was one of the best essays she received for that assignment! &gt;w&lt;</sub>
+<p>you can read the essay <a href="/writing/Necessary Phone Restrictions to Ensure Academic Achievement in the Digital Age.pdf">here</a>.</p>
