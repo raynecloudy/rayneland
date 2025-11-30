ee1784ef64fd416ab6c2bc76c00694efbae1f263
raynecloudy <contact@raynec.dev>
2025-11-30 10:09:31 -0800

    conceal biological sex

--- src/routes/(me)/(nav)/identity/+page.svelte
+++ src/routes/(me)/(nav)/identity/+page.svelte
@@ -7,7 +7,7 @@
 
 <svelte:head>
   <title>queer identity</title>
-  <meta name="og:description" content="if you know me personally, you probably know my pronouns and gender identity change a LOT. this page is to help you understand my gender and sexuality and stuff like that!">
+  <meta name="og:description" content="this page is to help you understand my gender and sexuality and stuff like that!">
   <meta name="og:title" content="queer identity">
   <meta name="og:type" content="website">
   <meta name="og:url" content="https://rayne.page/identity">
@@ -30,23 +30,21 @@
   </dropdown>
 </div>
 <h2>gender</h2>
-<p>if you know me personally, you probably know my pronouns and gender identity change a LOT. i'm so sorry, i can only imagine the pain of trying to keep up TwT here's the gender path i've been on:</p>
 <p>on the 18th of january, 2024, at around 11 PM, my egg cracked. i had just taken a gender identity test to see what i align with the most. the results were shocking, to say the least.</p>
 <img src="/egg_test.png" alt="test results">
 <p>according to this online test, i was 33% cisgender and <strong>86% transgender</strong>. initially, i wasn't sure how to react. i ended up identifying as non-binary soon after this revelation. i was male-leaning at this point in terms of gender expression.</p>
-<p>june is pride month. on the 1st of june, 2024, i came across transfeminine individuals online and related to a lot of what they were talking about. i was so sure i was transfem, and because of my impulsive nature, i came out as transfem online within 24 hours. my <a href="https://gamejolt.com/p/it-s-official-i-go-by-she-her-now-6gwermmq">coming out post</a> got 831 likes in just a few hours on a relatively niche platform. my phone was buzzing all day with notifications about it :sob:</p>
-<img src="/pronouns.jpg" alt="a social media post saying &quot;it's official, i go by she/her now&quot;">
-<p>on my 15th birthday at 11:47 PM, i came out as transfeminine to my family. i was shaking uncontrollably and crying because i was so scared of what they would think. i feel extremely grateful because they accepted me. i preferred the name "hayden" at this time and they began using it around the house. i filled out a form from the school to have my name changed in the system, but building up to the first day, i had extreme panic attacks and stress and worry and would often lie in bed crying. something was wrong.</p>
+<p>june is pride month. on the 1st of june, 2024, i came across transgender individuals online and related to a lot of what they were talking about. i was so sure i was also like them, and because of my impulsive nature, i came out as transgender online within 24 hours. my coming out post got 831 likes in just a few hours on a relatively niche platform. my phone was buzzing all day with notifications about it :sob:</p>
+<p>on my 15th birthday at 11:47 PM, i came out as transgender to my family. i was shaking uncontrollably and crying because i was so scared of what they would think. i feel extremely grateful because they accepted me. i preferred the name "hayden" at this time and they began using it around the house. i filled out a form from the school to have my name changed in the system, but building up to the first day, i had extreme panic attacks and stress and worry and would often lie in bed crying. something was wrong.</p>
 <p>on the first day of school, people asked me why my name was listed as "hayden." this made my anxiety even worse and i told all my peers and teachers that my name was actually [deadname] and that there was an issue with the system. once i had the chance, before i went home, i reverted the change in the system.</p>
-<p>i became "hayley" shortly after this. then "reese" (which, honestly, i'm still quite fond of....), and then "rayne." during the period of name changing, i stopped identifying as transfem, and instead as non-binary again. i've been rayne for quite a while now and i truly think this name is sticking.</p>
+<p>my name bounced around of a while after this before becoming "rayne." during the period of name changing, i stopped identifying as the opposite gender, and instead as non-binary again. i've been rayne for quite a while now and i truly think this name is sticking.</p>
 <p>my gender has since been bouncing around (f.e. i was bigender for a while) and i've officially given up trying to fit it into a neat little box.</p>
 <p>before the 2025-2026 school year, i changed my name in the system to "rayne." my teachers call me rayne, my friends are starting to call me rayne, and my immediate family will soon. so, where do we stand with my gender today?</p>
 <pages>
   <div>
-    <p>right now, i identify as <strong>unlabeled</strong>. this means i don't feel any personal attraction towards a specific label (such as <i>transfeminine</i> or <i>demiboy</i>).</p>
+    <p>right now, i identify as <strong>unlabeled</strong>. this means i don't feel any personal attraction towards a specific label.</p>
   </div>
   <div>
-    <p>as for gender <i>expression,</i> i identify as <strong>leaning feminine</strong>. i align myself to present feminine traits, like walking more femininely, wearing "girly" clothes, et cetera.</p>
+    <p>as for gender <i>expression,</i> i identify as <strong>leaning feminine</strong>. i like to present feminine traits, like walking more femininely, wearing "girly" clothes, et cetera.</p>
   </div>
 </pages>
 <img src="/lgbtq/unlabeled.svg" alt="unlabeled flag">
