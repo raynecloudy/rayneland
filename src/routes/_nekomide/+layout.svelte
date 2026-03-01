9b63e5e6a4feaeebfc8ecd76c32ca773ec861eab
raynecloudy <contact@raynec.dev>
2026-03-01 02:22:34 -0800

    remove nekomide favicon

--- src/routes/_nekomide/+layout.svelte
+++ src/routes/_nekomide/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  import favicon from "$lib/assets/favicon.svg";
+  // import favicon from "$lib/assets/favicon.svg";
 
   let { children } = $props();
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
+  <!-- <link rel="icon" href={favicon} /> -->
   <link rel="stylesheet" href="/styles/nekomide.css">
 </svelte:head>
 
