35163c1bb0dc05842be5fc4db6d18c7696cbfed6
raynecloudy <contact@raynec.dev>
2026-03-01 11:09:58 -0800

    okay

--- src/routes/api/presence/+server.ts
+++ src/routes/api/presence/+server.ts
@@ -1,17 +1,17 @@
-import { text } from "@sveltejs/kit";
-import { client, getStatus, setStatus } from "../../../hooks.server";
-import type { RequestHandler } from "./$types";
 import { env } from "$env/dynamic/private";
+import { text } from "@sveltejs/kit";
+import { Client, GatewayIntentBits } from "discord.js";
+import type { RequestHandler } from "./$types";
 
 export const GET: RequestHandler = async ({}) => {
-  if (!client.isReady()) {
-    await client.login(env.DISCORD_TOKEN);
-  }
-  const status = getStatus();
-  if (Date.now() - status.lastUpdated < 30_000) return text(status.type);
-  else {
-    const status = (await (await client.guilds.fetch("1327892346758762538")).members.fetch("1336737164691505246")).presence?.status ?? "unknown";
-    setStatus(status);
-    return text(status);
-  }
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildPresences
+    ]
+  });
+  await client.login(env.DISCORD_TOKEN);
+  const status = (await (await client.guilds.fetch("1327892346758762538")).members.fetch("1336737164691505246")).presence?.status ?? "unknown";
+  return text(status);
 };
