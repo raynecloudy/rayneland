dc722d8ae795b47ec57f795cd384b109d1f27365
raynecloudy <contact@raynec.dev>
2026-03-10 20:32:54 -0700

    haha

---
+++ src/routes/haha/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <meta property="og:type" content="video.other">
+  <meta property="og:video:url" content="https://rayne.page/haha">
+  <meta property="og:video:secure_url" content="https://rayne.page/haha">
+  <meta property="og:video:type" content="text/html">
+  <meta property="og:video:width" content="1280">
+  <meta property="og:video:height" content="720">
+</svelte:head>
+
+<div>haha</div>
