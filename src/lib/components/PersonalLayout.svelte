4c2fc2742a76935c2d7b82611462310613a802aa
raynecloudy <hi@rayne.page>
2026-05-16 15:07:58 -0700

    doodles

--- src/lib/components/PersonalLayout.svelte
+++ src/lib/components/PersonalLayout.svelte
@@ -44,44 +44,56 @@
   <link rel="stylesheet" href="/styles/personal.css">
 </svelte:head>
 
+<div style="position: absolute; top: 0; left: 0; pointer-events: none; width: 100vw; height: 100vh; opacity: 0.8;">
+  <img src="/media/doodles/swirl_left.png" aria-hidden="true" alt="" style="position: absolute; bottom: 2rem; left: 6rem;">
+  <img src="/media/doodles/swirl_right.png" aria-hidden="true" alt="" style="position: absolute; top: 5rem; right: 6rem;">
+</div>
+
 <main>
-  {#if page.url.searchParams.get("raynec.dev") === ""}
-    <section class="warning">
-      <p>hey, it looks like you found a link pointing to <strong>raynec.dev</strong>. that URL is deprecated. if you could do me a favor, contact the owner of the place you got this link from, and tell them to update it to <strong>rayne.page</strong>!</p>
-    </section>
-  {/if}
-  <section style:--image={`url(https://cdn.discordapp.com/banners/${discord.id}/${discord.banner}.png?size=512)`}>
-    <div id="me" class="flex">
-      <img src={`https://cdn.discordapp.com/avatars/${discord.id}/${discord.avatar}.webp?size=256`} alt="rayne">
-      <div>
-        <h1>Rayne <span class="grey">D.</span></h1>
-        <div class="flex">
-          {#if presence}
-            <div id="presence" class={presence.discord_status}><div></div>{presence.discord_status}</div>
-          {/if}
+  <div>
+    <div class="doodles">
+      <img src="/media/doodles/discord_left.png" aria-hidden="true" alt="" style="top: -1.5rem; left: -2rem;">
+      <img src="/media/doodles/discord_right.png" aria-hidden="true" alt="" style="top: 5.5rem; right: 1.5rem;">
+      <img src="/media/doodles/side.png" aria-hidden="true" alt="" style="top: 20rem; left: -4.5rem;">
+    </div>
+    <section style:--image={`url(https://cdn.discordapp.com/banners/${discord.id}/${discord.banner}.png?size=512)`}>
+      <div id="me" class="flex">
+        <img src={`https://cdn.discordapp.com/avatars/${discord.id}/${discord.avatar}.webp?size=256`} alt="rayne">
+        <div>
+          <h1>Rayne <span class="grey">D.</span></h1>
+          <div class="flex">
+            {#if presence}
+              <div id="presence" class={presence.discord_status}><div></div>{presence.discord_status}</div>
+            {/if}
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
   {@render children?.()}
 </main>
 <aside>
-  <section>
-    <ul>
-      <li class:highlighted={page.url.pathname === "/"}><a href="/">home</a></li>
-      <li class:highlighted={page.url.pathname === "/art"}><a href="/art">art</a></li>
-      <li class:highlighted={page.url.pathname === "/commissions"}><a href="/commissions">commissions</a></li>
-      <li class:highlighted={page.url.pathname === "/characters" || page.url.pathname.startsWith("/characters/")}><a href="/characters">characters</a></li>
-      <li class:highlighted={page.url.pathname === "/contact"}><a href="/contact">contact</a></li>
-      <li class:highlighted={page.url.pathname === "/projects"}><a href="/projects">projects</a></li>
-      <li><a href="//ai.rayne.page">rAIne</a></li>
-      <li><a href="//smpstats.rayne.page">RWS SMP stats</a></li>
-      <li class:highlighted={page.url.pathname === "/writing"}><a href="/writing">writing</a></li>
-    </ul>
-    <p>local time: {now.toLocaleTimeString("en-US", {
-      timeZone: "America/Los_Angeles"
-    })}</p>
-  </section>
+  <div>
+    <div class="doodles">
+      <img src="/media/doodles/corner.png" aria-hidden="true" alt="" style="top: -2.45rem; right: -2.7rem;">
+    </div>
+    <section>
+      <ul>
+        <li class:highlighted={page.url.pathname === "/"}><a href="/">home</a></li>
+        <li class:highlighted={page.url.pathname === "/art"}><a href="/art">art</a></li>
+        <li class:highlighted={page.url.pathname === "/commissions"}><a href="/commissions">commissions</a></li>
+        <li class:highlighted={page.url.pathname === "/characters" || page.url.pathname.startsWith("/characters/")}><a href="/characters">characters</a></li>
+        <li class:highlighted={page.url.pathname === "/contact"}><a href="/contact">contact</a></li>
+        <li class:highlighted={page.url.pathname === "/projects"}><a href="/projects">projects</a></li>
+        <li><a href="//ai.rayne.page">rAIne</a></li>
+        <li><a href="//smpstats.rayne.page">RWS SMP stats</a></li>
+        <li class:highlighted={page.url.pathname === "/writing"}><a href="/writing">writing</a></li>
+      </ul>
+      <p>local time: {now.toLocaleTimeString("en-US", {
+        timeZone: "America/Los_Angeles"
+      })}</p>
+    </section>
+  </div>
   {#if currentlyPlaying}
   <section id="music" style:--image="url({currentlyPlaying.image[3]["#text"]})">
       <div>
@@ -115,13 +127,18 @@
       <li><a href="https://toyhou.se/raynecloudy">toyhouse</a></li>
     </ul>
   </DropdownSection>
-  <table style:margin="auto">
-    <tbody>
-      <tr>
-        <td><a href="https://steve0greatness.github.io/webring/sites/breakfast/prev.xhtml">Prev</a></td>
-        <td><a href="https://steve0greatness.github.io/webring"><img src="/media/0greatness_webring.webp" width="100" height="42" alt="0Greatness Webring"></a></td>
-        <td><a href="https://steve0greatness.github.io/webring/sites/breakfast/next.xhtml">Next</a></td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <table style:margin="auto">
+      <tbody>
+        <tr>
+          <td><a href="https://steve0greatness.github.io/webring/sites/breakfast/prev.xhtml">Prev</a></td>
+          <td><a href="https://steve0greatness.github.io/webring"><img src="/media/0greatness_webring.webp" width="100" height="42" alt="0Greatness Webring"></a></td>
+          <td><a href="https://steve0greatness.github.io/webring/sites/breakfast/next.xhtml">Next</a></td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="doodles">
+      <img src="/media/doodles/bottom.png" aria-hidden="true" alt="" style="top: 1rem; left: 0.7rem;">
+    </div>
+  </div>
 </aside>
